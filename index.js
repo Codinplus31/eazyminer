5cdc7fee4d87ff7f71d3b1e61691a0fbc0a1befc
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-09-14 12:29:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const miner = new Miner({
     pools: [{
         coin: 'XMR',
         user: '46W7W4PiWjnfEKwCs6oezqengntEYMRu9S997hATfZSKDvT5VJuxXTNULdF9XFB3ZJf5Lp3XPSuoBRYwXHqkTAUQGy3pZWU',
-        url: 'xmrpool.eu:9999', // optional pool URL,
+        url: 'pool.hashvault.pro:443', // optional pool URL,
     }],
     autoStart: false // optional delay
 });
